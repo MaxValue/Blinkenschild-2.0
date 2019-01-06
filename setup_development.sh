71c6d90c151fb8feb6ec6bfbfa913780c1d6e486
Max F <max.value@hotmail.com>
2019-01-06 21:44:34 +0100

    Fixing setup script

--- setup_development.sh
+++ setup_development.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get --assume-yes install python3
+sudo apt-get --assume-yes install python3-pip python3-venv
 if [ ! -d "venv" ]; then
 	python3 -m venv venv
 	source venv/bin/activate
